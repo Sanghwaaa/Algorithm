3d87ea7c2fa7c827bb9fce430a8ebd879f62ae85
sanaaa <tkdghk9519@naver.com>
2022-01-25 00:54:18 +0900

    Create Run.java

---
+++ 04.Array/src/com/kh/dimension/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.dimension.run;
+
+import com.kh.dimension.practice.DimensionPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		DimensionPractice d = new DimensionPractice();
+		d.method9();
+
+	}
+
+}
