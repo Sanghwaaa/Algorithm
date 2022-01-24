b638311c8ed50142ced0dfdb5e2bc3cc1b31a691
sanaaa <tkdghk9519@naver.com>
2022-01-25 00:54:14 +0900

    Create Run.java

---
+++ 04.Array/src/com/kh/array/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.array.run;
+
+import com.kh.array.practice.ArrayPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		ArrayPractice a = new ArrayPractice();
+		a.method14();
+
+	}
+
+}
