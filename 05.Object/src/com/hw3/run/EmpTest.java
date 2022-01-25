84c32a5e59ad803867cf72f956393b49e324465d
sanaaa <tkdghk9519@naver.com>
2022-01-26 02:46:26 +0900

    Create EmpTest.java

---
+++ 05.Object/src/com/hw3/run/EmpTest.java
@@ -0,0 +1,14 @@
+package com.hw3.run;
+
+import com.hw3.view.EmpMenu;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		
+		EmpMenu e = new EmpMenu();
+		e.mainMenu();
+
+	}
+
+}
